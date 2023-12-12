336f7a486d34067b9c79c33002ab1b2004e4bc24
dev_one <micool1030@gmail.com>
2023-12-12 18:08:53 +0900

    [feat] 스프링에 converter 등록

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,13 @@ public class HelloController {
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
         log.info("### data = {} ", data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        log.info("ipPort IP = {}", ipPort.getIp());
+        log.info("ipPort PORT = {}", ipPort.getPort());
         return "ok";
     }
 }
